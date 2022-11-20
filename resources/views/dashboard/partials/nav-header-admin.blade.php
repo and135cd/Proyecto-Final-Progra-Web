72e0b54c6d97545e1ce3f18da95f4a67b4269405
Gabriela Ovalle Motta <gaby26@umes.edu.gt>
2022-11-20 15:08:20 -0600

    Modificación de algunas vistas según usuario

--- resources/views/dashboard/partials/nav-header-admin.blade.php
+++ resources/views/dashboard/partials/nav-header-admin.blade.php
@@ -13,6 +13,7 @@
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{url('/')}}">Inicio</a>
                 </li>
+                
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{route('productos.index')}}">Productos</a>
                 </li>
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,23 +1,38 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-                    {{ __('You are logged in!') }}
-                </div>
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <header>
+        @include('dashboard.partials.nav-header-user')
+    </header>
+    <div id="app">
+
+        <main class="py-4">
+            <div class="container">
+                <h1>Bienvenido a la Tienda Online</h1>
             </div>
-        </div>
+            
+        </main>
     </div>
-</div>
-@endsection
+</body>
+</html>
